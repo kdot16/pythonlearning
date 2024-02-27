5394eb0dc88417acbcc61875d3bfdf9fba7c8f94
Ye Paing <ypaing@squarespace.com>
2024-02-27 14:58:29 -0500

    02 problem 2 done

--- problems/02_VariablesControlIO/problem2.py
+++ problems/02_VariablesControlIO/problem2.py
@@ -1,3 +1,10 @@
-x = input('How much is in your bank account?')
+currentAmount = float(input('How much is in your bank account?'))
+interestRates = float(input('What is the monthly interest rate?'))
+monthsToSave = int(input('How many months do you plan saving?'))
 
-print(x)
+
+for month in range(monthsToSave):
+	currentAmount = currentAmount + (currentAmount * interestRates)
+	print('Month {} - {}'.format(month, currentAmount))
+
+print('Total amount of money after interest is: ' + str(currentAmount))
